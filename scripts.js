bcac1168c6fcdee4b8b0eec11c43d4e580eb2694
João Pedro Ferreira <167441421+htmlcssJP@users.noreply.github.com>
2024-11-10 15:25:17 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+import { adicionarItem } from "./js/adicionaritem.js";
+
+const formularioItem = document.getElementById("formulario-item");
+formularioItem.addEventListener("submit", adicionarItem);
+
